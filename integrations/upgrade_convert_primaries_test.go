5b5701b016c1a5428cebc2b771e31a8d3ce9f407
Jamie McAtamney <jmcatamney@pivotal.io>
2018-08-10 12:09:22 -0700

    Update the pending convert-primaries tests
    We still do not want to run these tests for now (and have added a
    comment explaining why), but this commit makes minor changes to the
    tests to keep them up-to-date with the code changes that have occurred
    since the tests were marked as pending.
    
    Co-authored-by: Kevin Yeap <kyeap@pivotal.io>
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- integrations/upgrade_convert_primaries_test.go
+++ integrations/upgrade_convert_primaries_test.go
@@ -16,27 +16,38 @@ import (
 )
 
 var _ = Describe("upgrade convert primaries", func() {
+	/*
+	 * The two pending tests in this block are useful tests in theory, but
+	 * would rely on using an actual checklist manager over a mock checklist
+	 * manager due to the way status checking is done for pg_upgrade.
+	 *
+	 * If convert-primaries were modified to somehow use the actual checklist
+	 * manager, these tests would be meaningful and can be refactored and
+	 * fixed up appropriately.  Otherwise, it would make more sense to
+	 * replace these with end-to-end tests, as in that case they would grow
+	 * beyond the scope of integration tests due to a lack of mocking.
+	 *
+	 * TODO: Decide how to handle convert-primaries testing.
+	 */
 	var (
 		oidFile string
 	)
 
 	BeforeEach(func() {
 		var err error
-		segmentDataDir := os.Getenv("MASTER_DATA_DIRECTORY")
-		Expect(segmentDataDir).ToNot(Equal(""), "MASTER_DATA_DIRECTORY needs to be set!")
 
-		err = os.MkdirAll(filepath.Join(testStateDir, "pg_upgrade"), 0700)
+		err = os.MkdirAll(filepath.Join(testStateDir, "convert-master"), 0700)
 		Expect(err).ToNot(HaveOccurred())
-
-		oidFile = filepath.Join(testStateDir, "pg_upgrade", "pg_upgrade_dump_seg1_oids.sql")
-		f, err := os.Create(oidFile)
-		Expect(err).ToNot(HaveOccurred())
-		f.Close()
+		for i := range []int{0, 1, 2} {
+			oidFile = filepath.Join(testStateDir, "convert-master", fmt.Sprintf("pg_upgrade_dump_seg%d_oids.sql", i))
+			f, err := os.Create(oidFile)
+			Expect(err).ToNot(HaveOccurred())
+			f.Close()
+		}
 
 		go agent.Start()
 	})
 
-	// Move this elsewhere; it's not testing what's useful anymore.
 	XIt("updates status PENDING to RUNNING then to COMPLETE if successful", func() {
 		utils.System.RunCommandAsync = func(cmdStr string, logFile string) error {
 			_, err := agentExecutor.ExecuteLocalCommand(cmdStr)
@@ -45,7 +56,7 @@ var _ = Describe("upgrade convert primaries", func() {
 
 		cm.AddStep(upgradestatus.CONVERT_PRIMARIES, pb.UpgradeSteps_CONVERT_PRIMARIES)
 
-		Expect(runStatusUpgrade()).To(ContainSubstring("PENDING - Primary segment upgrade"))
+		Expect(runStatusUpgrade()).To(ContainSubstring("PENDING - Run pg_upgrade on primaries"))
 		testExecutor.LocalOutput = "TEST"
 
 		step := cm.GetStepWriter(upgradestatus.START_AGENTS)
@@ -57,16 +68,17 @@ var _ = Describe("upgrade convert primaries", func() {
 		upgradeConvertPrimaries := runCommand(
 			"upgrade",
 			"convert-primaries",
-			"--old-bindir", "/old/bindir",
-			"--new-bindir", "/new/bindir",
 		)
 		Expect(upgradeConvertPrimaries).To(Exit(0))
 
+		step = cm.GetStepWriter(upgradestatus.CONVERT_PRIMARIES)
+		step.MarkInProgress()
+
 		agentExecutor.LocalOutput = "pgrep for running pg_upgrade for segment"
-		Expect(runStatusUpgrade()).To(ContainSubstring("RUNNING - Primary segment upgrade"))
+		Expect(runStatusUpgrade()).To(ContainSubstring("RUNNING - Run pg_upgrade on primaries"))
 
 		for i := range []int{0, 1, 2} {
-			f, err := os.Create(filepath.Join(testStateDir, "pg_upgrade", fmt.Sprintf("seg-%d", i), ".done"))
+			f, err := os.Create(filepath.Join(testStateDir, upgradestatus.CONVERT_PRIMARIES, fmt.Sprintf("seg%d", i), ".done"))
 			Expect(err).ToNot(HaveOccurred())
 			f.Write([]byte("Upgrade complete\n"))
 			f.Close()
@@ -83,15 +95,14 @@ var _ = Describe("upgrade convert primaries", func() {
 
 		// Return no PIDs when pgrep checks if pg_upgrade is running
 		agentExecutor.LocalOutput = ""
-		Expect(runStatusUpgrade()).To(ContainSubstring("COMPLETE - Primary segment upgrade"))
+		Expect(runStatusUpgrade()).To(ContainSubstring("COMPLETE - Run pg_upgrade on primaries"))
 	})
 
-	// Move this elsewhere; it's not testing what's useful anymore.
 	XIt("updates status to FAILED if convert primaries fails on at least 1 agent", func() {
 		cm.AddStep(upgradestatus.CONVERT_PRIMARIES, pb.UpgradeSteps_CONVERT_PRIMARIES)
 
-		Expect(runStatusUpgrade()).To(ContainSubstring("PENDING - Primary segment upgrade"))
-		setStateFile(testStateDir, "pg_upgrade/seg-0", "1.failed")
+		Expect(runStatusUpgrade()).To(ContainSubstring("PENDING - Run pg_upgrade on primaries"))
+		setStateFile(testStateDir, "convert-primaries/seg0", "1.failed")
 
 		step := cm.GetStepWriter(upgradestatus.START_AGENTS)
 		step.MarkInProgress()
@@ -100,12 +111,10 @@ var _ = Describe("upgrade convert primaries", func() {
 		upgradeConvertPrimaries := runCommand(
 			"upgrade",
 			"convert-primaries",
-			"--old-bindir", "/old/bindir",
-			"--new-bindir", "/new/bindir",
 		)
 		Expect(upgradeConvertPrimaries).Should(Exit(0))
 
-		Expect(runStatusUpgrade()).To(ContainSubstring("FAILED - Primary segment upgrade"))
+		Expect(runStatusUpgrade()).To(ContainSubstring("FAILED - Run pg_upgrade on primaries"))
 	})
 })
 
