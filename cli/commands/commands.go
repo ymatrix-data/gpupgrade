03971950af202b9ba01d4b03cd89b90818774cbe
David Krieger <dkrieger@pivotal.io>
2018-09-28 17:14:35 -0700

    name-only refactor to make the command/subcommand structure better.

--- cli/commands/commands.go
+++ cli/commands/commands.go
@@ -1,5 +1,33 @@
 package commands
 
+/*
+ *  This file generates the command-line cli that is the heart of gpupgrade.  It uses Cobra to generate
+ *    the cli based on commands and sub-commands. The below in this comment block shows a notional example
+ *    of how this looks to give you an idea of what the command structure looks like at the cli.  It is NOT necessarily
+ *    up-to-date but is a useful as an orientation to what is going on here.
+ *
+ * example> gpupgrade
+ * 	   2018/09/28 16:09:39 Please specify one command of: check, config, prepare, status, upgrade, or version
+ *
+ * example> gpupgrade check
+ *      collects information and validates the target Greenplum installation can be upgraded
+ *
+ *      Usage:
+ * 		gpupgrade check [command]
+ *
+ * 		Available Commands:
+ * 			config       gather cluster configuration
+ * 			disk-space   check that disk space usage is less than 80% on all segments
+ * 			object-count count database objects and numeric objects
+ * 			seginstall   confirms that the new software is installed on all segments
+ * 			version      validate current version is upgradable
+ *
+ * 		Flags:
+ * 			-h, --help   help for check
+ *
+ * 		Use "gpupgrade check [command] --help" for more information about a command.
+*/
+
 import (
 	"context"
 	"fmt"
@@ -19,20 +47,26 @@ import (
 )
 
 func BuildRootCommand() *cobra.Command {
+
+	// TODO: if called without a subcommand, the cli prints a help message with timestamp.  Remove the timestamp.
 	root := &cobra.Command{Use: "gpupgrade"}
 
 	root.AddCommand(prepare, config, status, check, version, upgrade)
 
-	subInit := createInitSubcommand()
-	prepare.AddCommand(subStartHub, subInitCluster, subShutdownClusters, subStartAgents, subInit)
+	subPrepareInit := createPrepareInitSubcommand()
+	prepare.AddCommand(subPrepareStartHub, subPrepareInitCluster, subPrepareShutdownClusters, subPrepareStartAgents,
+			subPrepareInit)
 
-	subSet := createSetSubcommand()
-	subShow := createShowSubcommand()
-	config.AddCommand(subSet, subShow)
+	subConfigSet := createConfigSetSubcommand()
+	subConfigShow := createConfigShowSubcommand()
+	config.AddCommand(subConfigSet, subConfigShow)
 
-	status.AddCommand(subUpgrade, subConversion)
-	check.AddCommand(subVersion, subObjectCount, subDiskSpace, subConfig, subSeginstall)
-	upgrade.AddCommand(subConvertMaster, subConvertPrimaries, subShareOids, subValidateStartCluster, subReconfigurePorts)
+	status.AddCommand(subStatusUpgrade, subStatusConversion)
+
+	check.AddCommand(subCheckVersion, subCheckObjectCount, subCheckDiskSpace, subCheckConfig, subCheckSeginstall)
+
+	upgrade.AddCommand(subUpgradeConvertMaster, subUpgradeConvertPrimaries, subUpgradeShareOids,
+			subUpgradeValidateStartCluster, subUpgradeReconfigurePorts)
 
 	return root
 }
@@ -91,114 +125,82 @@ func connectToHub() pb.CliToHubClient {
 	return pb.NewCliToHubClient(conn)
 }
 
-var prepare = &cobra.Command{
-	Use:   "prepare",
-	Short: "subcommands to help you get ready for a gpupgrade",
-	Long:  "subcommands to help you get ready for a gpupgrade",
-}
-
-var status = &cobra.Command{
-	Use:   "status",
-	Short: "subcommands to show the status of a gpupgrade",
-	Long:  "subcommands to show the status of a gpupgrade",
-}
-
+//////////////////////////////////////// CHECK and its subcommands
 var check = &cobra.Command{
 	Use:   "check",
 	Short: "collects information and validates the target Greenplum installation can be upgraded",
 	Long:  `collects information and validates the target Greenplum installation can be upgraded`,
 }
 
-var version = &cobra.Command{
-	Use:   "version",
-	Short: "Version of gpupgrade",
-	Long:  `Version of gpupgrade`,
+var subCheckConfig = &cobra.Command{
+	Use:   "config",
+	Short: "gather cluster configuration",
+	Long:  "gather cluster configuration",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(commanders.VersionString())
+		client := connectToHub()
+		err := commanders.NewConfigChecker(client).Execute()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subCheckDiskSpace = &cobra.Command{
+	Use:     "disk-space",
+	Short:   "check that disk space usage is less than 80% on all segments",
+	Long:    "check that disk space usage is less than 80% on all segments",
+	Aliases: []string{"du"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := connectToHub()
+		return commanders.NewDiskSpaceChecker(client).Execute()
+	},
+}
+var subCheckObjectCount = &cobra.Command{
+	Use:     "object-count",
+	Short:   "count database objects and numeric objects",
+	Long:    "count database objects and numeric objects",
+	Aliases: []string{"oc"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := connectToHub()
+		return commanders.NewObjectCountChecker(client).Execute()
+	},
+}
+var subCheckSeginstall = &cobra.Command{
+	Use:   "seginstall",
+	Short: "confirms that the new software is installed on all segments",
+	Long: "Running this command will validate that the new software is installed on all segments, " +
+		"and register successful or failed validation (available in `gpupgrade status upgrade`)",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewSeginstallChecker(client).Execute()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Seginstall is underway. Use command \"gpupgrade status upgrade\" " +
+			"to check its current status, and/or hub logs for possible errors.")
+	},
+}
+var subCheckVersion = &cobra.Command{
+	Use:     "version",
+	Short:   "validate current version is upgradable",
+	Long:    `validate current version is upgradable`,
+	Aliases: []string{"ver"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client := connectToHub()
+		return commanders.NewVersionChecker(client).Execute()
 	},
 }
 
-var upgrade = &cobra.Command{
-	Use:   "upgrade",
-	Short: "starts upgrade process",
-	Long:  `starts upgrade process`,
-}
-
+//////////////////////////////////////// CONFIG and its subcommands
 var config = &cobra.Command{
 	Use:   "config",
 	Short: "subcommands to set parameters for subsequent gpupgrade commands",
 	Long:  "subcommands to set parameters for subsequent gpupgrade commands",
 }
 
-var subStartHub = &cobra.Command{
-	Use:   "start-hub",
-	Short: "starts the hub",
-	Long:  "starts the hub",
-	Run: func(cmd *cobra.Command, args []string) {
-		preparer := commanders.Preparer{}
-		err := preparer.StartHub()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-
-		client := connectToHub()
-		err = preparer.VerifyConnectivity(client)
-
-		if err != nil {
-			gplog.Error("gpupgrade is unable to connect via gRPC to the hub")
-			gplog.Error("%v", err)
-			os.Exit(1)
-		}
-	},
-}
-
-var subShutdownClusters = &cobra.Command{
-	Use:   "shutdown-clusters",
-	Short: "shuts down both old and new cluster",
-	Long:  "Current assumptions is both clusters exist.",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		preparer := commanders.NewPreparer(client)
-		err := preparer.ShutdownClusters()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subStartAgents = &cobra.Command{
-	Use:   "start-agents",
-	Short: "start agents on segment hosts",
-	Long:  "start agents on all segments",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		preparer := commanders.NewPreparer(client)
-		err := preparer.StartAgents()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subInitCluster = &cobra.Command{
-	Use:   "init-cluster",
-	Short: "inits the cluster",
-	Long:  "Current assumptions is that the cluster already exists. And will only generate json config file for now.",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		preparer := commanders.NewPreparer(client)
-		err := preparer.InitCluster()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-func createSetSubcommand() *cobra.Command {
+func createConfigSetSubcommand() *cobra.Command {
 	subSet := &cobra.Command{
 		Use:   "set",
 		Short: "set an upgrade parameter",
@@ -236,7 +238,7 @@ func createSetSubcommand() *cobra.Command {
 	return subSet
 }
 
-func createShowSubcommand() *cobra.Command {
+func createConfigShowSubcommand() *cobra.Command {
 	subShow := &cobra.Command{
 		Use:   "show",
 		Short: "show configuration settings",
@@ -286,173 +288,14 @@ func createShowSubcommand() *cobra.Command {
 	return subShow
 }
 
-var subUpgrade = &cobra.Command{
-	Use:   "upgrade",
-	Short: "the status of the upgrade",
-	Long:  "the status of the upgrade",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		reporter := commanders.NewReporter(client)
-		err := reporter.OverallUpgradeStatus()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
+//////////////////////////////////////// PREPARE and its subcommands
+var prepare = &cobra.Command{
+	Use:   "prepare",
+	Short: "subcommands to help you get ready for a gpupgrade",
+	Long:  "subcommands to help you get ready for a gpupgrade",
 }
 
-var subVersion = &cobra.Command{
-	Use:     "version",
-	Short:   "validate current version is upgradable",
-	Long:    `validate current version is upgradable`,
-	Aliases: []string{"ver"},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		client := connectToHub()
-		return commanders.NewVersionChecker(client).Execute()
-	},
-}
-
-var subObjectCount = &cobra.Command{
-	Use:     "object-count",
-	Short:   "count database objects and numeric objects",
-	Long:    "count database objects and numeric objects",
-	Aliases: []string{"oc"},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		client := connectToHub()
-		return commanders.NewObjectCountChecker(client).Execute()
-	},
-}
-
-var subDiskSpace = &cobra.Command{
-	Use:     "disk-space",
-	Short:   "check that disk space usage is less than 80% on all segments",
-	Long:    "check that disk space usage is less than 80% on all segments",
-	Aliases: []string{"du"},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		client := connectToHub()
-		return commanders.NewDiskSpaceChecker(client).Execute()
-	},
-}
-
-var subConversion = &cobra.Command{
-	Use:   "conversion",
-	Short: "the status of the conversion",
-	Long:  "the status of the conversion",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		reporter := commanders.NewReporter(client)
-		err := reporter.OverallConversionStatus()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subConfig = &cobra.Command{
-	Use:   "config",
-	Short: "gather cluster configuration",
-	Long:  "gather cluster configuration",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewConfigChecker(client).Execute()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subSeginstall = &cobra.Command{
-	Use:   "seginstall",
-	Short: "confirms that the new software is installed on all segments",
-	Long: "Running this command will validate that the new software is installed on all segments, " +
-		"and register successful or failed validation (available in `gpupgrade status upgrade`)",
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewSeginstallChecker(client).Execute()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-
-		fmt.Println("Seginstall is underway. Use command \"gpupgrade status upgrade\" " +
-			"to check its current status, and/or hub logs for possible errors.")
-	},
-}
-
-var subConvertMaster = &cobra.Command{
-	Use:   "convert-master",
-	Short: "start upgrade process on master",
-	Long:  `start upgrade process on master`,
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewUpgrader(client).ConvertMaster()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subConvertPrimaries = &cobra.Command{
-	Use:   "convert-primaries",
-	Short: "start upgrade process on primary segments",
-	Long:  `start upgrade process on primary segments`,
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewUpgrader(client).ConvertPrimaries()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subShareOids = &cobra.Command{
-	Use:   "share-oids",
-	Short: "share oid files across cluster",
-	Long:  `share oid files generated by pg_upgrade on master, across cluster`,
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewUpgrader(client).ShareOids()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subValidateStartCluster = &cobra.Command{
-	Use:   "validate-start-cluster",
-	Short: "Attempt to start upgraded cluster",
-	Long:  `Use gpstart in order to validate that the new cluster can successfully transition from a stopped to running state`,
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewUpgrader(client).ValidateStartCluster()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-var subReconfigurePorts = &cobra.Command{
-	Use:   "reconfigure-ports",
-	Short: "Set master port on upgraded cluster to the value from the older cluster",
-	Long:  `Set master port on upgraded cluster to the value from the older cluster`,
-	Run: func(cmd *cobra.Command, args []string) {
-		client := connectToHub()
-		err := commanders.NewUpgrader(client).ReconfigurePorts()
-		if err != nil {
-			gplog.Error(err.Error())
-			os.Exit(1)
-		}
-	},
-}
-
-// gpupgrade prepare init
-func createInitSubcommand() *cobra.Command {
+func createPrepareInitSubcommand() *cobra.Command {
 	var oldBinDir, newBinDir string
 
 	subInit := &cobra.Command{
@@ -475,4 +318,187 @@ func createInitSubcommand() *cobra.Command {
 	subInit.MarkPersistentFlagRequired("new-bindir")
 
 	return subInit
+}
+var subPrepareInitCluster = &cobra.Command{
+	Use:   "init-cluster",
+	Short: "inits the cluster",
+	Long:  "Current assumptions is that the cluster already exists. And will only generate json config file for now.",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		preparer := commanders.NewPreparer(client)
+		err := preparer.InitCluster()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subPrepareShutdownClusters = &cobra.Command{
+	Use:   "shutdown-clusters",
+	Short: "shuts down both old and new cluster",
+	Long:  "Current assumptions is both clusters exist.",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		preparer := commanders.NewPreparer(client)
+		err := preparer.ShutdownClusters()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subPrepareStartAgents = &cobra.Command{
+	Use:   "start-agents",
+	Short: "start agents on segment hosts",
+	Long:  "start agents on all segments",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		preparer := commanders.NewPreparer(client)
+		err := preparer.StartAgents()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subPrepareStartHub = &cobra.Command{
+	Use:   "start-hub",
+	Short: "starts the hub",
+	Long:  "starts the hub",
+	Run: func(cmd *cobra.Command, args []string) {
+		preparer := commanders.Preparer{}
+		err := preparer.StartHub()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+
+		client := connectToHub()
+		err = preparer.VerifyConnectivity(client)
+
+		if err != nil {
+			gplog.Error("gpupgrade is unable to connect via gRPC to the hub")
+			gplog.Error("%v", err)
+			os.Exit(1)
+		}
+	},
+}
+
+//////////////////////////////////////// STATUS and its subcommands
+var status = &cobra.Command{
+	Use:   "status",
+	Short: "subcommands to show the status of a gpupgrade",
+	Long:  "subcommands to show the status of a gpupgrade",
+}
+
+var subStatusConversion = &cobra.Command{
+	Use:   "conversion",
+	Short: "the status of the conversion",
+	Long:  "the status of the conversion",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		reporter := commanders.NewReporter(client)
+		err := reporter.OverallConversionStatus()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subStatusUpgrade = &cobra.Command{
+	Use:   "upgrade",
+	Short: "the status of the upgrade",
+	Long:  "the status of the upgrade",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		reporter := commanders.NewReporter(client)
+		err := reporter.OverallUpgradeStatus()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+//////////////////////////////////////// UPGRADE and its subcommands
+var upgrade = &cobra.Command{
+	Use:   "upgrade",
+	Short: "starts upgrade process",
+	Long:  `starts upgrade process`,
+}
+
+var subUpgradeConvertMaster = &cobra.Command{
+	Use:   "convert-master",
+	Short: "start upgrade process on master",
+	Long:  `start upgrade process on master`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewUpgrader(client).ConvertMaster()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subUpgradeConvertPrimaries = &cobra.Command{
+	Use:   "convert-primaries",
+	Short: "start upgrade process on primary segments",
+	Long:  `start upgrade process on primary segments`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewUpgrader(client).ConvertPrimaries()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subUpgradeReconfigurePorts = &cobra.Command{
+	Use:   "reconfigure-ports",
+	Short: "Set master port on upgraded cluster to the value from the older cluster",
+	Long:  `Set master port on upgraded cluster to the value from the older cluster`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewUpgrader(client).ReconfigurePorts()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subUpgradeShareOids = &cobra.Command{
+	Use:   "share-oids",
+	Short: "share oid files across cluster",
+	Long:  `share oid files generated by pg_upgrade on master, across cluster`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewUpgrader(client).ShareOids()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+var subUpgradeValidateStartCluster = &cobra.Command{
+	Use:   "validate-start-cluster",
+	Short: "Attempt to start upgraded cluster",
+	Long:  `Use gpstart in order to validate that the new cluster can successfully transition from a stopped to running state`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := connectToHub()
+		err := commanders.NewUpgrader(client).ValidateStartCluster()
+		if err != nil {
+			gplog.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+//////////////////////////////////////// VERSION
+var version = &cobra.Command{
+	Use:   "version",
+	Short: "Version of gpupgrade",
+	Long:  `Version of gpupgrade`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(commanders.VersionString())
+	},
 }
