9c3e8bb4ef67206479ab77218bf0a4f7d66977fb
Kalen Krempely <kkrempely@pivotal.io>
2020-01-28 19:21:16 -0800

    Remove dead code
    Removes unused code RunCommandAsync and WriteJSONFile.

--- utils/sys_utils.go
+++ utils/sys_utils.go
@@ -1,17 +1,11 @@
 package utils
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"os/user"
 	"path/filepath"
 	"time"
-
-	"github.com/greenplum-db/gp-common-go-libs/gplog"
-	"github.com/pkg/errors"
 )
 
 var (
@@ -27,68 +21,45 @@ var (
  */
 
 type SystemFunctions struct {
-	CurrentUser     func() (*user.User, error)
-	Getenv          func(key string) string
-	Getpid          func() int
-	Hostname        func() (string, error)
-	IsNotExist      func(err error) bool
-	MkdirAll        func(path string, perm os.FileMode) error
-	Now             func() time.Time
-	Open            func(name string) (*os.File, error)
-	OpenFile        func(name string, flag int, perm os.FileMode) (*os.File, error)
-	Remove          func(name string) error
-	RemoveAll       func(name string) error
-	Rename          func(oldpath, newpath string) error
-	ReadFile        func(filename string) ([]byte, error)
-	WriteFile       func(filename string, data []byte, perm os.FileMode) error
-	Stat            func(name string) (os.FileInfo, error)
-	FilePathGlob    func(pattern string) ([]string, error)
-	Create          func(name string) (*os.File, error)
-	RunCommandAsync func(cmdStr, logFile string) error
+	CurrentUser  func() (*user.User, error)
+	Getenv       func(key string) string
+	Getpid       func() int
+	Hostname     func() (string, error)
+	IsNotExist   func(err error) bool
+	MkdirAll     func(path string, perm os.FileMode) error
+	Now          func() time.Time
+	Open         func(name string) (*os.File, error)
+	OpenFile     func(name string, flag int, perm os.FileMode) (*os.File, error)
+	Remove       func(name string) error
+	RemoveAll    func(name string) error
+	Rename       func(oldpath, newpath string) error
+	ReadFile     func(filename string) ([]byte, error)
+	WriteFile    func(filename string, data []byte, perm os.FileMode) error
+	Stat         func(name string) (os.FileInfo, error)
+	FilePathGlob func(pattern string) ([]string, error)
+	Create       func(name string) (*os.File, error)
 }
 
 func InitializeSystemFunctions() *SystemFunctions {
 	return &SystemFunctions{
-		CurrentUser:     user.Current,
-		Getenv:          os.Getenv,
-		Getpid:          os.Getpid,
-		Hostname:        os.Hostname,
-		IsNotExist:      os.IsNotExist,
-		MkdirAll:        os.MkdirAll,
-		Now:             time.Now,
-		Open:            os.Open,
-		OpenFile:        os.OpenFile,
-		Remove:          os.Remove,
-		RemoveAll:       os.RemoveAll,
-		Rename:          os.Rename,
-		Stat:            os.Stat,
-		FilePathGlob:    filepath.Glob,
-		ReadFile:        ioutil.ReadFile,
-		WriteFile:       ioutil.WriteFile,
-		Create:          os.Create,
-		RunCommandAsync: RunCommandAsync,
+		CurrentUser:  user.Current,
+		Getenv:       os.Getenv,
+		Getpid:       os.Getpid,
+		Hostname:     os.Hostname,
+		IsNotExist:   os.IsNotExist,
+		MkdirAll:     os.MkdirAll,
+		Now:          time.Now,
+		Open:         os.Open,
+		OpenFile:     os.OpenFile,
+		Remove:       os.Remove,
+		RemoveAll:    os.RemoveAll,
+		Rename:       os.Rename,
+		Stat:         os.Stat,
+		FilePathGlob: filepath.Glob,
+		ReadFile:     ioutil.ReadFile,
+		WriteFile:    ioutil.WriteFile,
+		Create:       os.Create,
 	}
-}
-
-func RunCommandAsync(cmdStr, logFile string) error {
-	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		errMsg := fmt.Sprintf("mkdir %s failed: %v.", logFile, err)
-		gplog.Error(errMsg)
-		return errors.New(errMsg)
-	}
-
-	cmd := exec.Command("bash", "-c", cmdStr)
-	cmd.Stdout = f
-	cmd.Stderr = f
-
-	err = cmd.Start()
-	if err != nil {
-		errMsg := fmt.Sprintf("Command %s failed to run: %s", cmdStr, err)
-		gplog.Error(errMsg)
-		return errors.New(errMsg)
-	}
-	return nil
 }
 
 func TryEnv(varname string, defval string) string {
@@ -119,30 +90,4 @@ func GetStateDir() string {
 	}
 
 	return stateDir
-}
-
-func WriteJSONFile(fileName string, contents interface{}) error {
-	body, err := json.Marshal(contents)
-	if err != nil {
-		return err
-	}
-
-	// Write to a temporary file and move it over the old one, because WriteFile
-	// will not truncate the original file, and it provides an atomic write
-	// FIXME: What happens if this file already exists?
-	tempFilePath := fileName + ".tmp"
-	err = System.WriteFile(tempFilePath, body, 0644)
-	if err != nil {
-		errMsg := fmt.Sprintf("Unable to write temp file %s: %s", tempFilePath, err.Error())
-		return errors.New(errMsg)
-	}
-	defer os.Remove(tempFilePath)
-
-	err = os.Rename(tempFilePath, fileName)
-	if err != nil {
-		errMsg := fmt.Sprintf("Unable to rename temp file to %s: %s", fileName, err.Error())
-		return errors.New(errMsg)
-	}
-
-	return nil
 }
--- utils/sys_utils_test.go
+++ utils/sys_utils_test.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
 	"os/user"
 
 	"github.com/pkg/errors"
@@ -91,108 +88,5 @@ var _ = Describe("user utils", func() {
 			})
 		})
 
-	})
-
-	Describe("#WriteJSONFile", func() {
-		var (
-			dir      string
-			fileName string
-		)
-
-		BeforeEach(func() {
-			var err error
-			dir, err = ioutil.TempDir("", "")
-			Expect(err).ToNot(HaveOccurred())
-
-			fileName = dir + "/upgrade_settings.json"
-		})
-
-		AfterEach(func() {
-			err := os.RemoveAll(dir)
-			Expect(err).ToNot(HaveOccurred())
-		})
-
-		readJSON := func(fileName string) map[string]string {
-			_, err := os.Open(fileName)
-			Expect(err).ToNot(HaveOccurred())
-
-			contents, err := System.ReadFile(fileName)
-			Expect(err).ToNot(HaveOccurred())
-
-			result := make(map[string]string)
-			err = json.Unmarshal([]byte(contents), &result)
-			Expect(err).ToNot(HaveOccurred())
-
-			return result
-		}
-
-		It("writes a map to a json file", func() {
-			expected := map[string]string{}
-			expected["someFlag"] = "some-value"
-
-			err := WriteJSONFile(fileName, expected)
-			Expect(err).ToNot(HaveOccurred())
-
-			result := readJSON(fileName)
-			Expect(expected).To(Equal(result))
-		})
-
-		It("writes map to a existing file, correctly truncating old contents", func() {
-			f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			Expect(err).ToNot(HaveOccurred())
-
-			brotherSays := `...And Saint Attila raised the hand grenade up on high,
-			saying, "O LORD, bless this Thy hand grenade that with it Thou mayest
-			blow Thine enemies to tiny bits, in Thy mercy." And the LORD did grin and
-			the people did feast upon the lambs and sloths and carp and anchovies and
-			orangutans and breakfast cereals, and fruit bats and large chu ---`
-
-			_, err = f.Write([]byte(brotherSays))
-			Expect(err).ToNot(HaveOccurred())
-
-			err = f.Close()
-			Expect(err).ToNot(HaveOccurred())
-
-			expected := map[string]string{}
-			expected["someFlag"] = "some-value"
-
-			err = WriteJSONFile(fileName, expected)
-			Expect(err).ToNot(HaveOccurred())
-
-			result := readJSON(fileName)
-			Expect(result).To(Equal(expected))
-		})
-
-		It("fails when passing an object that can't be json.Marshal()'d", func() {
-			badStruct := map[interface{}]string{}
-			key := struct{}{}
-			badStruct[key] = "dummy_val"
-
-			err := WriteJSONFile(fileName, badStruct)
-			Expect(err).To(HaveOccurred())
-		})
-
-		// XXX: This is an implementation specific regression test
-		It("doesn't remove temp file if exists and write fails", func() {
-			tempFileName := fileName + ".tmp"
-
-			f, err := os.OpenFile(tempFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			Expect(err).ToNot(HaveOccurred())
-
-			err = f.Close()
-			Expect(err).ToNot(HaveOccurred())
-
-			failedUpdate := map[string]string{}
-			failedUpdate["someFlag"] = "updated-value"
-
-			System.WriteFile = func(_ string, _ []byte, _ os.FileMode) error {
-				return errors.New("Mock write failed")
-			}
-			err = WriteJSONFile(fileName, failedUpdate)
-			Expect(err).To(HaveOccurred())
-
-			_, err = os.Stat(tempFileName)
-			Expect(err).ToNot(HaveOccurred())
-		})
 	})
 })
