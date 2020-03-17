9d8557fdf72c4fa587716b656590ba7d47ccf9f3
Kalen Krempely <kkrempely@pivotal.io>
2020-03-17 16:13:21 -0700

    Migrate panic_test.go to Go style
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- utils/log/panic_test.go
+++ utils/log/panic_test.go
@@ -1,39 +1,29 @@
 package log_test
 
 import (
-	"github.com/greenplum-db/gpupgrade/utils/log"
+	"strings"
+	"testing"
 
-	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"github.com/greenplum-db/gp-common-go-libs/testhelper"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gbytes"
+	"github.com/greenplum-db/gpupgrade/utils/log"
 )
 
-var _ = Describe("WritePanics", func() {
-	It("does not swallow panics", func() {
-		panicFunc := func() {
-			defer log.WritePanics()
-			panic("ahhh")
-		}
-		Expect(panicFunc).To(Panic())
+func TestWritePanics(t *testing.T) {
+	t.Run("writes panics to the log file", func(t *testing.T) {
+		_, _, buffer := testhelper.SetupTestLogger()
+
+		expected := "ahhh"
+		defer func() {
+			if e := recover(); e != nil {
+				contents := string(buffer.Contents())
+				if !strings.Contains(contents, expected) {
+					t.Errorf("expected %q in log file: %q", expected, contents)
+				}
+			}
+		}()
+
+		defer log.WritePanics()
+		panic(expected)
 	})
-
-	It("writes panic information to the gplog file only", func() {
-		oldLogger := gplog.GetLogger()
-		defer func() { gplog.SetLogger(oldLogger) }()
-
-		testout, testerr, testlog := testhelper.SetupTestLogger()
-
-		panicMsg := "aaahhhhh"
-		Expect(func() {
-			defer log.WritePanics()
-			panic(panicMsg)
-		}).To(Panic())
-
-		Expect(testout.Contents()).To(BeEmpty())
-		Expect(testerr.Contents()).To(BeEmpty())
-		Expect(testlog).To(Say(`encountered panic \("%s"\); stack trace follows`, panicMsg))
-	})
-})
+}
