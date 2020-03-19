3680b10989dc0cd55d9743a642e9da1f56252663
Kalen Krempely <kkrempely@pivotal.io>
2020-03-18 17:41:29 -0700

    Migrate hub_test.go to Go style test
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

--- integrations/hub_test.go
+++ integrations/hub_test.go
@@ -1,41 +1,100 @@
 package integrations_test
 
 import (
+	"fmt"
 	"os/exec"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
 
 	"github.com/greenplum-db/gpupgrade/cli/commanders"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/greenplum-db/gpupgrade/testutils"
 )
 
-var _ = Describe("gpupgrade hub", func() {
+const HubPort = 7527
 
-	// XXX We should be testing the locally built artifacts, and killing only
-	// hubs that are started as part of this test. The current logic will break
-	// functional installed systems.
-	BeforeEach(func() {
-		killHub()
-	})
+func TestHub(t *testing.T) {
+	t.Run("the hub does not daemonizes unless --deamonize is passed", func(t *testing.T) {
+		killHub(t)
+		defer killHub(t)
 
-	AfterEach(func() {
-		killHub()
-	})
+		dir := testutils.GetTempDir(t, "")
+		defer testutils.MustRemoveAll(t, dir)
 
-	It("does not daemonize unless explicitly told to", func() {
+		resetEnv := testutils.SetEnv(t, "GPUPGRADE_HOME", filepath.Join(dir, ".gpupgrade"))
+		defer resetEnv()
+
 		err := commanders.CreateStateDir()
-		Expect(err).ToNot(HaveOccurred())
+		if err != nil {
+			t.Errorf("unexpected error got %#v", err)
+		}
+
 		err = commanders.CreateInitialClusterConfigs()
-		Expect(err).ToNot(HaveOccurred())
+		if err != nil {
+			t.Errorf("unexpected error got %#v", err)
+		}
 
 		cmd := exec.Command("gpupgrade", "hub")
-		done := make(chan error, 1)
+		errChan := make(chan error, 1)
 
 		go func() {
-			// We expect this to never return.
-			done <- cmd.Run()
+			errChan <- cmd.Run() // expected to never return
 		}()
 
-		Consistently(done).ShouldNot(Receive())
+		select {
+		case err := <-errChan:
+			if err != nil {
+				t.Errorf("unexpected error %#v", err)
+			}
+		case <-time.After(100 * time.Millisecond):
+			// hub daemonizes without an error
+		}
 	})
-})
+}
+
+// killHub finds all running hub processes and kills them.
+// XXX we should really use a PID file for this, and allow side-by-side hubs,
+// rather than blowing away developer state.
+func killHub(t *testing.T) {
+	t.Helper()
+
+	defer func() {
+		if isPortInUse(HubPort) {
+			t.Errorf("hub port %d is not available", HubPort)
+		}
+	}()
+
+	killCommand := exec.Command("pkill", "-f", "^gpupgrade hub")
+	err := killCommand.Run()
+	// pkill returns exit code 1 if no processes were matched, which is fine.
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		if exitErr.ExitCode() == 1 {
+			return
+		}
+	}
+
+	if err != nil {
+		t.Errorf("unexpected error got %#v", err)
+	}
+}
+
+func isPortInUse(port int) bool {
+	t := time.After(2 * time.Second)
+	select {
+	case <-t:
+		fmt.Println("timed out")
+		break
+	default:
+		cmd := exec.Command("/bin/sh", "-c", "'lsof | grep "+strconv.Itoa(port)+"'")
+		err := cmd.Run()
+		output, _ := cmd.CombinedOutput()
+		if _, ok := err.(*exec.ExitError); ok && string(output) == "" {
+			return false
+		}
+
+		time.Sleep(250 * time.Millisecond)
+	}
+
+	return true
+}
--- integrations/integrations_suite_test.go
+++ integrations/integrations_suite_test.go
@@ -2,45 +2,23 @@ package integrations_test
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
-	"testing"
-	"time"
 
-	multierror "github.com/hashicorp/go-multierror"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/hashicorp/go-multierror"
 )
 
-func TestCommands(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Integration Tests Suite")
-}
-
-var (
-	testWorkspaceDir string // will be recreated for every test
-	testStateDir     string // what would normally be ~/.gpupgrade
-)
-
-const (
-	cliToHubPort = 7527
-)
-
-var _ = BeforeSuite(func() {
+func init() {
 	// All gpupgrade binaries are expected to be on the path for integration
 	// tests. Be nice to developers and check up front; warn if the binaries
 	// being tested aren't contained in a directory directly above this test
 	// file.
 	_, testPath, _, ok := runtime.Caller(0)
 	if !ok {
-		Fail("couldn't retrieve Caller() information")
+		panic("couldn't retrieve Caller() information")
 	}
 
 	var allErrs error
@@ -57,62 +35,9 @@ var _ = BeforeSuite(func() {
 		}
 	}
 	if allErrs != nil {
-		Fail(fmt.Sprintf(
+		panic(fmt.Sprintf(
 			"Please put gpupgrade binaries on your PATH before running integration tests.\n%s",
 			multierror.Flatten(allErrs),
 		))
 	}
-})
-
-// BeforeEach for the integrations suite will create the testWorkspaceDir and
-// testStateDir for tests. GPUPGRADE_HOME is set to the testStateDir, so tests
-// may run the hub without worrying about colliding with the developer
-// environment. Both directories are removed in the suite AfterEach.
-var _ = BeforeEach(func() {
-	var err error
-	testWorkspaceDir, err = ioutil.TempDir("", "")
-	Expect(err).ToNot(HaveOccurred())
-	testStateDir = filepath.Join(testWorkspaceDir, ".gpupgrade")
-	os.Setenv("GPUPGRADE_HOME", testStateDir)
-})
-
-var _ = AfterEach(func() {
-	os.RemoveAll(testWorkspaceDir)
-})
-
-// killHub finds all running hub processes and kills them.
-// XXX we should really use a PID file for this, and allow side-by-side hubs,
-// rather than blowing away developer state.
-func killHub() {
-	killCommand := exec.Command("pkill", "-f", "^gpupgrade hub")
-	err := killCommand.Run()
-
-	// pkill returns exit code 1 if no processes were matched, which is fine.
-	if err != nil {
-		Expect(err).To(MatchError("exit status 1"))
-	} else {
-		Expect(err).ToNot(HaveOccurred())
-	}
-
-	Expect(checkPortIsAvailable(cliToHubPort)).To(BeTrue())
-}
-
-func checkPortIsAvailable(port int) bool {
-	t := time.After(2 * time.Second)
-	select {
-	case <-t:
-		fmt.Println("timed out")
-		break
-	default:
-		cmd := exec.Command("/bin/sh", "-c", "'lsof | grep "+strconv.Itoa(port)+"'")
-		err := cmd.Run()
-		output, _ := cmd.CombinedOutput()
-		if _, ok := err.(*exec.ExitError); ok && string(output) == "" {
-			return true
-		}
-
-		time.Sleep(250 * time.Millisecond)
-	}
-
-	return false
 }
