e4110e22bb185be69cd08cc05b2b46366087743e
Jacob Champion <pchampion@pivotal.io>
2020-03-31 12:44:07 -0700

    CI: introduce AllVersions concept
    The template Data.AllVersions consists of a combined and deduplicated
    list of source and target versions. Its utility in this patch is limited
    to the declaration of binary resources in the pipeline, but this will
    allow us to make the 6-to-6 job a mundane case instead of a special one.

--- ci/parser/parse_template.go
+++ ci/parser/parse_template.go
@@ -18,6 +18,7 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"text/template"
 
 	"github.com/blang/semver"
@@ -35,6 +36,7 @@ type UpgradeJob struct {
 
 type Data struct {
 	SourceVersions, TargetVersions []string
+	AllVersions                    []string // combination of Source/Target
 	UpgradeJobs                    []*UpgradeJob
 	LastTargetVersion              string
 	PrimariesOnly                  []bool
@@ -67,13 +69,37 @@ func init() {
 			})
 		}
 	}
+
 	// Duplicate version data here in order to simplify template logic
 	data = Data{
 		SourceVersions:    sourceVersions,
 		TargetVersions:    targetVersions,
+		AllVersions:       deduplicate(sourceVersions, targetVersions),
 		UpgradeJobs:       upgradeJobs,
 		LastTargetVersion: targetVersions[len(targetVersions)-1],
 	}
+}
+
+// deduplicate combines, sorts, and deduplicates two string slices.
+func deduplicate(a, b []string) []string {
+	var all []string
+
+	all = append(all, a...)
+	all = append(all, b...)
+	sort.Strings(all)
+
+	// Deduplicate by compacting runs of identical strings.
+	cur := 0
+	for next := 1; next < len(all); next++ {
+		if all[cur] == all[next] {
+			continue
+		}
+
+		cur++
+		all[cur] = all[next]
+	}
+
+	return all[:cur+1]
 }
 
 func main() {
