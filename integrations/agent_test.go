605f48df7debd8ebb8ddba23dbf766215ed5f331
Kalen Krempely <kkrempely@pivotal.io>
2020-05-20 19:34:02 -0700

    fix agent test for default port
    Don't specify --port when testing the default.
    
    By switching to cmd.CombinedOutput() and daemonizing the agent we
    correctly bubble up errors if the agent fails to start. This also
    drastically speeds up test time from 1 to 0.04 seconds. In order to
    correctly kill the daemonized agent we need to enable process groups and
    kill all processes in the group. We do so by sending a kill with -PGID
    which in this case is the same as -PID.

--- integrations/agent_test.go
+++ integrations/agent_test.go
@@ -10,54 +10,62 @@ import (
 	"testing"
 	"time"
 
+	"golang.org/x/sys/unix"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/connectivity"
 
 	"github.com/greenplum-db/gpupgrade/testutils"
 	"github.com/greenplum-db/gpupgrade/upgrade"
 )
 
 func TestAgent(t *testing.T) {
+	port := testutils.MustGetPort(t)
+
 	cases := []struct {
-		name string
-		port int
+		name     string
+		expected int
+		args     []string
 	}{
 		{
-			name: "agent starts on default port",
-			port: upgrade.DefaultAgentPort,
+			name:     "agent starts on default port",
+			expected: upgrade.DefaultAgentPort,
+			args:     []string{"agent", "--daemonize"},
 		},
 		{
-			name: "agent starts on specified port",
-			port: testutils.MustGetPort(t),
+			name:     "agent starts on specified port",
+			expected: port,
+			args:     []string{"agent", "--daemonize", "--port", strconv.Itoa(port)},
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			cmd := exec.Command("gpupgrade", "agent", "--port", strconv.Itoa(c.port))
-			err := cmd.Start()
+			cmd := exec.Command("gpupgrade", c.args...)
+
+			// in order to kill the daemonized process enable process groups
+			cmd.SysProcAttr = &unix.SysProcAttr{Setpgid: true}
+			output, err := cmd.CombinedOutput()
 			defer func() {
-				if err := cmd.Process.Kill(); err != nil {
+				// To kill the daemonized process we need to kill all processes
+				// in the group by sending a kill signal to -PGID. In this case,
+				// since the PID is the same as the PGID we can send -PID.
+				if err := unix.Kill(-cmd.Process.Pid, unix.SIGTERM); err != nil {
 					t.Fatalf("failed to kill gpupgrade agent: %+v", err)
 				}
 			}()
 			if err != nil {
 				t.Errorf("gpupgrade agent returned unexpected error %+v", err)
+				t.Logf("output:\n%s", string(output))
 			}
 
 			// connect to the specified port
-			timeout := 2 * time.Second
+			timeout := 1 * time.Second
 			ctx, cancel := context.WithTimeout(context.Background(), timeout)
 			defer cancel()
 
-			address := "localhost" + ":" + strconv.Itoa(c.port)
-			conn, err := grpc.DialContext(ctx, address, grpc.WithInsecure(), grpc.WithBlock())
+			address := "localhost" + ":" + strconv.Itoa(c.expected)
+			_, err = grpc.DialContext(ctx, address, grpc.WithInsecure(), grpc.WithBlock())
 			if err != nil {
 				t.Fatalf("failed to connect to address %s %+v", address, err)
-			}
-
-			if conn.GetState() != connectivity.Ready {
-				t.Errorf("got %s want Ready", conn.GetState())
 			}
 		})
 	}
