35de6646dd1d2fdee5bf71c9172432fd9d2f1e45
Kalen Krempely <kkrempely@pivotal.io>
2020-06-22 15:43:50 -0700

    remove CreateDataDirectory which is unused

--- utils/sys_utils.go
+++ utils/sys_utils.go
@@ -10,16 +10,11 @@ import (
 	"os/user"
 	"path/filepath"
 	"time"
-
-	"github.com/greenplum-db/gp-common-go-libs/gplog"
-	"golang.org/x/xerrors"
 )
 
 var (
 	System = InitializeSystemFunctions()
 )
-
-const markerFile = ".gpupgrade"
 
 /*
  * SystemFunctions holds function pointers for built-in functions that will need
@@ -100,35 +95,6 @@ func GetLogDir() (string, error) {
 
 func GetArchiveDirectoryName(t time.Time) string {
 	return t.Format("gpupgrade-2006-01-02T15:04")
-}
-
-func CreateDataDirectory(dataDir string) error {
-	file := filepath.Join(dataDir, markerFile)
-	_, err := System.Stat(file)
-	if err == nil {
-		err = System.RemoveAll(dataDir)
-		if err != nil {
-			return xerrors.Errorf("remove data directory %s: %w", dataDir, err)
-		}
-	}
-
-	if !os.IsNotExist(err) && err != nil {
-		return xerrors.Errorf("stat marker file %s: %w", markerFile, err)
-	}
-
-	gplog.Info("creating directory %s", dataDir)
-	err = System.Mkdir(dataDir, 0755)
-	if err != nil {
-		return xerrors.Errorf("create data directory %s: %w", dataDir, err)
-	}
-
-	mFile := filepath.Join(dataDir, markerFile)
-	gplog.Info("creating marker file %s", mFile)
-	err = System.WriteFile(mFile, []byte{}, 0644)
-	if err != nil {
-		return xerrors.Errorf("create gpupgrade marker file %s: %w", mFile, err)
-	}
-	return nil
 }
 
 func GetTablespaceDir() string {
