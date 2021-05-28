47bc22ca36b54a7c93b683b547e6bbd0bfc9ef14
Kalen Krempely <kkrempely@vmware.com>
2021-05-28 13:05:52 -0700

    pg_upgrade: add test for partitions using keywords
    Ensure that partition names having keywords (reserved, non-reserved and
    unclassified) can be upgraded by quoting them using the quote_all_identifiers
    GUC.
    
    For a comprehensive list of keywords see:
    https://www.postgresql.org/docs/8.3/sql-keywords-appendix.html
    
    Co-authored-by: Soumyadeep Chakraborty <soumyadeep2007@gmail.com>

---
+++ test/acceptance/pg_upgrade/upgradeable_tests/source_cluster_regress/sql/partitions_using_keywords.sql
@@ -0,0 +1,30 @@
+-- Copyright (c) 2017-2021 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+-- Ensure that partition names having keywords (reserved, non-reserved and
+-- unclassified) can be upgraded by quoting them using the quote_all_identifiers
+-- GUC.
+
+--------------------------------------------------------------------------------
+-- Create and setup upgradeable objects
+--------------------------------------------------------------------------------
+
+--
+-- Create a partitioned table using reserved (ie: "window"), non-reserved
+-- (ie: "current"), and unclassified (ie: "allocate") keywords for partition names.
+-- For a comprehensive list of keywords:
+-- https://www.postgresql.org/docs/8.3/sql-keywords-appendix.html
+CREATE TABLE t_quote_test (a int, b int, c int, d int, e text)
+    DISTRIBUTED BY (a)
+    PARTITION BY RANGE (b)
+        SUBPARTITION BY RANGE (c)
+            SUBPARTITION TEMPLATE (
+            START (1) END (2) EVERY (1),
+            DEFAULT SUBPARTITION "current" )
+        SUBPARTITION BY LIST (e)
+            SUBPARTITION TEMPLATE (
+            SUBPARTITION "allocate" VALUES ('val1'),
+            SUBPARTITION "window" VALUES ('val2'),
+            DEFAULT SUBPARTITION dsp )
+        ( START (2002) END (2003) EVERY (1),
+        DEFAULT PARTITION dp );
---
+++ test/acceptance/pg_upgrade/upgradeable_tests/target_cluster_regress/sql/partitions_using_keywords.sql
@@ -0,0 +1,8 @@
+-- Copyright (c) 2017-2021 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+--------------------------------------------------------------------------------
+-- Validate that the upgradeable objects are functional post-upgrade
+--------------------------------------------------------------------------------
+
+SELECT pg_get_partition_def('t_quote_test'::regclass);
