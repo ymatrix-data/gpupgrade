34ba321542fc7a7eb56df010dd68113938187a41
Kalen Krempely <kkrempely@vmware.com>
2021-07-19 11:48:38 -0700

    ci: refactor pg-upgrade-tests.bash
    Place code in functions for clarity.

--- ci/scripts/pg-upgrade-tests.bash
+++ ci/scripts/pg-upgrade-tests.bash
@@ -7,87 +7,98 @@ set -eux -o pipefail
 
 # NOTE: All these steps need to be done in the same task since each task is run
 # in its own isolated container with no shared state. Thus, installing the RPM,
-# or making isolation2 would not be shared between tasks (and thus containers).
+# and making isolation2 needs to be done in the same task/container.
 
 export GPHOME_SOURCE=/usr/local/greenplum-db-source
 export GPHOME_TARGET=/usr/local/greenplum-db-target
 
-################################################################################
-# Install BATS
-################################################################################
-./bats/install.sh /usr/local
+install_source_GPDB_rpm_and_symlink() {
+    yum install -y rpm_gpdb_source/*.rpm
 
-################################################################################
-# Install gpupgrade RPM
-################################################################################
-yum install -y rpm_enterprise/gpupgrade-*.rpm
+    version=$(rpm -q --qf '%{version}' "$SOURCE_PACKAGE" | tr _ -)
+    ln -s /usr/local/greenplum-db-${version} "$GPHOME_SOURCE"
 
-################################################################################
-# Install source and target GPDB RPMs
-################################################################################
-# setup gpadmin user
-./gpdb_src/concourse/scripts/setup_gpadmin_user.bash "centos"
+    chown -R gpadmin:gpadmin "$GPHOME_SOURCE"
+}
 
-# install source packages
-yum install -y rpm_gpdb_source/*.rpm
-
-version=$(rpm -q --qf '%{version}' "$SOURCE_PACKAGE" | tr _ -)
-ln -s /usr/local/greenplum-db-${version} "$GPHOME_SOURCE"
-
-chown -R gpadmin:gpadmin "$GPHOME_SOURCE"
-
-# install target packages
 # XXX: Setup target cluster before sourcing greenplum_path otherwise there are
 # yum errors due to python issues.
 # XXX: When source equals target then yum will fail when trying to re-install.
-if [ "$SOURCE_PACKAGE" != "$TARGET_PACKAGE" ]; then
-  yum install -y rpm_gpdb_target/*.rpm
-fi
+install_target_GPDB_rpm_and_symlink() {
+    if [ "$SOURCE_PACKAGE" != "$TARGET_PACKAGE" ]; then
+      yum install -y rpm_gpdb_target/*.rpm
+    fi
 
-version=$(rpm -q --qf '%{version}' "$TARGET_PACKAGE" | tr _ -)
-ln -s /usr/local/greenplum-db-${version} "$GPHOME_TARGET"
+    version=$(rpm -q --qf '%{version}' "$TARGET_PACKAGE" | tr _ -)
+    ln -s /usr/local/greenplum-db-${version} "$GPHOME_TARGET"
 
-chown -R gpadmin:gpadmin "$GPHOME_TARGET"
+    chown -R gpadmin:gpadmin "$GPHOME_TARGET"
+}
 
-################################################################################
-# Make pg_isolation2_regress for target version
-################################################################################
-# setup_configure_vars and configure expect GPHOME=/usr/local/greenplum-db-devel
-# Thus, symlink the target version to /usr/local/greenplum-db-devel.
-# Alternatively, refactor common.bash to use $GPHOME. However, due to unforeseen
-# consequences and stability concerns we cannot do that.
-ln -s "$GPHOME_TARGET" /usr/local/greenplum-db-devel
-set +u
-source gpdb_src/concourse/scripts/common.bash
-setup_configure_vars
-export LDFLAGS="-L$GPHOME_TARGET/ext/python/lib $LDFLAGS"
-configure
-set -u
+make_pg_isolation2_regress_for_the_target_GPDB_version() {
+    # setup_configure_vars and configure expect GPHOME=/usr/local/greenplum-db-devel
+    # Thus, symlink the target version to /usr/local/greenplum-db-devel.
+    # Alternatively, refactor common.bash to use $GPHOME. However, due to unforeseen
+    # consequences and stability concerns we cannot do that.
+    ln -s "$GPHOME_TARGET" /usr/local/greenplum-db-devel
+    set +u
+    source gpdb_src/concourse/scripts/common.bash
+    setup_configure_vars
+    export LDFLAGS="-L$GPHOME_TARGET/ext/python/lib $LDFLAGS"
+    configure
+    set -u
 
-source "${GPHOME_TARGET}"/greenplum_path.sh
-make -j "$(nproc)" -C gpdb_src
-make -j "$(nproc)" -C gpdb_src/src/test/isolation2 install
+    source "${GPHOME_TARGET}"/greenplum_path.sh
+    make -j "$(nproc)" -C gpdb_src
+    make -j "$(nproc)" -C gpdb_src/src/test/isolation2 install
+}
 
-################################################################################
-# Create source cluster
-################################################################################
-source "$GPHOME_SOURCE"/greenplum_path.sh
+create_source_cluster() {
+    source "$GPHOME_SOURCE"/greenplum_path.sh
 
-chown -R gpadmin:gpadmin gpdb_src_source/gpAux/gpdemo
-su gpadmin -c "make -j $(nproc) -C gpdb_src_source/gpAux/gpdemo create-demo-cluster"
-source gpdb_src_source/gpAux/gpdemo/gpdemo-env.sh
+    chown -R gpadmin:gpadmin gpdb_src_source/gpAux/gpdemo
+    su gpadmin -c "make -j $(nproc) -C gpdb_src_source/gpAux/gpdemo create-demo-cluster"
+    source gpdb_src_source/gpAux/gpdemo/gpdemo-env.sh
+}
 
-################################################################################
-# Run tests
-################################################################################
-chown -R gpadmin:gpadmin gpupgrade_src
+run_pg_upgrade_tests() {
+    chown -R gpadmin:gpadmin gpupgrade_src
+    time su gpadmin -c '
+        set -eux -o pipefail
 
-su gpadmin -c "
-    set -eux -o pipefail
+        export TERM=linux
+        export ISOLATION2_PATH=$(readlink -e gpdb_src/src/test/isolation2)
 
-    export TERM=linux
-    export ISOLATION2_PATH=$(readlink -e gpdb_src/src/test/isolation2)
+        cd gpupgrade_src
+        make pg-upgrade-tests
+    '
+}
 
-    cd gpupgrade_src
-    make pg-upgrade-tests
-"
+main() {
+    echo "Installing BATS..."
+    ./bats/install.sh /usr/local
+
+    echo "Installing gpupgrade rpm..."
+    yum install -y rpm_enterprise/gpupgrade-*.rpm
+
+    echo "Setting up gpadmin user..."
+    mkdir -p gpdb_src
+    ./gpdb_src_source/concourse/scripts/setup_gpadmin_user.bash "centos"
+
+    echo "Installing the source GPDB rpm and symlink..."
+    install_source_GPDB_rpm_and_symlink
+
+    echo "Installing the target GPDB rpm and symlink..."
+    install_target_GPDB_rpm_and_symlink
+
+    echo "Making pg_isolation2_regress for the target GPDB version..."
+    make_pg_isolation2_regress_for_the_target_GPDB_version
+
+    echo "Creating the source demo cluster..."
+    create_source_cluster
+
+    echo "Running tests..."
+    run_pg_upgrade_tests
+}
+
+main
