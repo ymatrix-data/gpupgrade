7f8b94e43ae2e5d3629589b95729f3383f8ace80
Orhan Kislal <okislal@apache.org>
2021-10-11 17:46:35 -0400

    Update data-migration-script comment on partitions with constraints

--- data-migration-scripts/test/create_nonupgradable_objects.sql
+++ data-migration-scripts/test/create_nonupgradable_objects.sql
@@ -76,7 +76,9 @@ INSERT INTO table_with_primary_constraint VALUES(2, 2);
 CREATE ROLE gphdfs_user CREATEEXTTABLE(protocol='gphdfs', type='writable') CREATEEXTTABLE(protocol='gphdfs', type='readable');
 
 -- create partitioned tables where the index relation name is not equal primary/unique key constraint name for the root
-
+-- Note that the naming of the constraint is key, not the type of constraint
+-- If the constraint is named, every partition will have the same named constraint and they all can be dropped with the same command
+-- If the constraint is not named, greenplum generates a unique name for each partition as well as the master table. We can only drop the master tables constraint and the partition constraints remain in effect
 DROP TABLE IF EXISTS table_with_unique_constraint_p;
 CREATE TYPE table_with_unique_constraint_p_author_key AS (dummy int);
 CREATE TYPE table_with_unique_constraint_p_author_key1 AS (dummy int);
