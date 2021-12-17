0a722689bda0bd779dba472ad48bf48b61e47969
Kalen Krempely <kkrempely@vmware.com>
2021-12-17 09:34:21 -0800

    add-semver: work with first and later tagged commits
    Also, add back the hard coded “-1” packing build number as this is the
    convention for rpmbuild.

--- ci/scripts/add-semver.bash
+++ ci/scripts/add-semver.bash
@@ -13,20 +13,24 @@ function rename_rpm() {
 
     rpm=$(basename "${release}_rpm"/gpupgrade-*.rpm)
     # shellcheck disable=2001
-    sed s/gpupgrade-intermediate/gpupgrade-"${SEMVER}"/ <<< "$rpm"
+    # NOTE: The "-1" is the packing build number which enables us to release
+    # a new package when there are no code changes.
+    sed s/gpupgrade-intermediate/gpupgrade-"${SEMVER}-1"/ <<< "$rpm"
 }
 
 # "git describe" does not return a semver compatible version that can correctly be used with Concourse to
 # ensure pulling the correct artifact. So "0.4.0-32-g763a08e5" becomes "0.4.0+dev.32.g973669ba".
 git_describe=$(git -C ./gpupgrade_src describe --tags)
 IFS='-' read -r -a parts <<< "$git_describe"
-SEMVER="${parts[0]}"
-if (( ${#parts[@]} == 3 )); then
-    SEMVER="${SEMVER}+dev.${parts[1]}.${parts[2]}"
+if (( ${#parts[@]} == 1 )); then
+    SEMVER="${parts[0]}"
+    cp oss_rpm/gpupgrade-*.rpm renamed_oss_rpm/"$(rename_rpm oss)"
+    cp enterprise_rpm/gpupgrade-*.rpm renamed_enterprise_rpm/"$(rename_rpm enterprise)"
+elif (( ${#parts[@]} == 3 )); then
+    SEMVER="${parts[0]}+dev.${parts[1]}.${parts[2]}"
+    cp oss_rpm/gpupgrade-*.rpm renamed_oss_rpm/"$(rename_rpm oss)"
+    cp enterprise_rpm/gpupgrade-*.rpm renamed_enterprise_rpm/"$(rename_rpm enterprise)"
 else
-    echo "git describe '${git_describe}' was split into ${#parts[@]} parts [${parts[*]}]. Expected 3."
+    echo "git describe '${git_describe}' was split into ${#parts[@]} parts [${parts[*]}]. Expected 1 or 3."
     exit 1
 fi
-
-cp oss_rpm/gpupgrade-*.rpm renamed_oss_rpm/"$(rename_rpm oss)"
-cp enterprise_rpm/gpupgrade-*.rpm renamed_enterprise_rpm/"$(rename_rpm enterprise)"
