c61d7b11ec33eef1f6fd9cbcf17697ee7f3e33dd
Orhan Kislal <okislal@apache.org>
2022-02-09 16:39:21 -0500

    Remove redundant index creation

--- data-migration-scripts/post-finalize/recreate_indexes_on_deprecated_built_in_types.sql
+++ data-migration-scripts/post-finalize/recreate_indexes_on_deprecated_built_in_types.sql
@@ -40,6 +40,7 @@ WHERE
               AND NOT attisdropped
         )
     AND c.relkind = 'r'
+    AND c.relhassubclass != TRUE
     AND xc.relkind = 'i'
     AND n.nspname NOT LIKE 'pg_temp_%'
     AND n.nspname NOT LIKE 'pg_toast_temp_%'
