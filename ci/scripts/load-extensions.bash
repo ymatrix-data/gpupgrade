230a8a9b496ec6975cf0e30b16e49c4a4641c059
Kalen Krempely <kkrempely@vmware.com>
2022-03-10 16:41:51 -0800

    CI: move pxf next to GPDB extensions
    Place installing all GPDB extensions together, and then install postgres
    native extensions.

--- ci/scripts/load-extensions.bash
+++ ci/scripts/load-extensions.bash
@@ -134,78 +134,6 @@ SQL_EOF
 
 test_plcontainer "$OS_VERSION" && install_plcontainer || echo "Skipping plcontainer for centos6 since its not supported..."
 
-echo "Installing postgres native extensions and sample data on source cluster..."
-time ssh -n mdw "
-    set -eux -o pipefail
-
-    source /usr/local/greenplum-db-source/greenplum_path.sh
-
-    echo 'Installing amcheck...'
-    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
-        CREATE EXTENSION amcheck;
-
-        CREATE VIEW amcheck_test_view AS
-          SELECT bt_index_check(c.oid)::TEXT, c.relpages
-          FROM pg_index i
-          JOIN pg_opclass op ON i.indclass[0] = op.oid
-          JOIN pg_am am ON op.opcmethod = am.oid
-          JOIN pg_class c ON i.indexrelid = c.oid
-          JOIN pg_namespace n ON c.relnamespace = n.oid
-          WHERE am.amname = 'btree' AND n.nspname = 'pg_catalog'
-            -- Function may throw an error when this is omitted:
-            AND i.indisready AND i.indisvalid
-          ORDER BY c.relpages DESC LIMIT 10;
-SQL_EOF
-
-    echo 'Installing dblink...'
-    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
-        \i /usr/local/greenplum-db-source/share/postgresql/contrib/dblink.sql
-
-        CREATE TABLE foo(f1 int, f2 text, primary key (f1,f2));
-        INSERT INTO foo VALUES (0,'a');
-        INSERT INTO foo VALUES (1,'b');
-        INSERT INTO foo VALUES (2,'c');
-        CREATE VIEW dblink_test_view AS SELECT * FROM dblink('dbname=postgres', 'SELECT * FROM foo') AS t(a int, b text) WHERE t.a > 7;
-SQL_EOF
-
-    echo 'Installing hstore...'
-    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
-        \i /usr/local/greenplum-db-source/share/postgresql/contrib/hstore.sql
-
-        CREATE TABLE hstore_test_type AS SELECT 'a=>1,a=>2'::hstore as c1;
-        CREATE VIEW hstore_test_view AS SELECT c1 -> 'a' as c2 FROM hstore_test_type;
-SQL_EOF
-
-    echo 'Installing pgcrypto...'
-    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
-        CREATE EXTENSION pgcrypto;
-
-        CREATE VIEW pgcrypto_test_view AS SELECT crypt('new password', gen_salt('md5'));
-SQL_EOF
-
-    echo 'Installing Fuzzy String Match...'
-    psql -d postgres -f /usr/local/greenplum-db-source/share/postgresql/contrib/fuzzystrmatch.sql
-    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
-        CREATE VIEW fuzzystrmatch_test_view AS SELECT soundex('a'::text);
-SQL_EOF
-
-    echo 'Installing citext...'
-    echo 'Create a new db to avoid potential function overlaps with postgis to simplify the diff when comparing before and after upgrade.'
-    createdb citext_db
-    psql -d citext_db -f /usr/local/greenplum-db-source/share/postgresql/contrib/citext.sql
-    psql -v ON_ERROR_STOP=1 -d citext_db <<SQL_EOF
-        CREATE TABLE citext_test_type (
-            id bigint PRIMARY KEY,
-            nick CITEXT NOT NULL,
-            pass TEXT   NOT NULL
-        ) DISTRIBUTED BY (id);
-
-        INSERT INTO citext_test_type VALUES (1,  'larry',  md5(random()::text) );
-SQL_EOF
-
-
-"
-
 install_pxf() {
     local PXF_BASE=/home/gpadmin/pxf
 
@@ -281,6 +209,76 @@ SQL_EOF
 }
 
 test_pxf "$OS_VERSION" && install_pxf || echo "Skipping pxf for centos6 since pxf6 for GPDB6 on centos6 is not supported..."
+
+echo "Installing postgres native extensions and sample data on source cluster..."
+time ssh -n mdw "
+    set -eux -o pipefail
+
+    source /usr/local/greenplum-db-source/greenplum_path.sh
+
+    echo 'Installing amcheck...'
+    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
+        CREATE EXTENSION amcheck;
+
+        CREATE VIEW amcheck_test_view AS
+          SELECT bt_index_check(c.oid)::TEXT, c.relpages
+          FROM pg_index i
+          JOIN pg_opclass op ON i.indclass[0] = op.oid
+          JOIN pg_am am ON op.opcmethod = am.oid
+          JOIN pg_class c ON i.indexrelid = c.oid
+          JOIN pg_namespace n ON c.relnamespace = n.oid
+          WHERE am.amname = 'btree' AND n.nspname = 'pg_catalog'
+            -- Function may throw an error when this is omitted:
+            AND i.indisready AND i.indisvalid
+          ORDER BY c.relpages DESC LIMIT 10;
+SQL_EOF
+
+    echo 'Installing dblink...'
+    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
+        \i /usr/local/greenplum-db-source/share/postgresql/contrib/dblink.sql
+
+        CREATE TABLE foo(f1 int, f2 text, primary key (f1,f2));
+        INSERT INTO foo VALUES (0,'a');
+        INSERT INTO foo VALUES (1,'b');
+        INSERT INTO foo VALUES (2,'c');
+        CREATE VIEW dblink_test_view AS SELECT * FROM dblink('dbname=postgres', 'SELECT * FROM foo') AS t(a int, b text) WHERE t.a > 7;
+SQL_EOF
+
+    echo 'Installing hstore...'
+    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
+        \i /usr/local/greenplum-db-source/share/postgresql/contrib/hstore.sql
+
+        CREATE TABLE hstore_test_type AS SELECT 'a=>1,a=>2'::hstore as c1;
+        CREATE VIEW hstore_test_view AS SELECT c1 -> 'a' as c2 FROM hstore_test_type;
+SQL_EOF
+
+    echo 'Installing pgcrypto...'
+    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
+        CREATE EXTENSION pgcrypto;
+
+        CREATE VIEW pgcrypto_test_view AS SELECT crypt('new password', gen_salt('md5'));
+SQL_EOF
+
+    echo 'Installing Fuzzy String Match...'
+    psql -d postgres -f /usr/local/greenplum-db-source/share/postgresql/contrib/fuzzystrmatch.sql
+    psql -v ON_ERROR_STOP=1 -d postgres <<SQL_EOF
+        CREATE VIEW fuzzystrmatch_test_view AS SELECT soundex('a'::text);
+SQL_EOF
+
+    echo 'Installing citext...'
+    echo 'Create a new db to avoid potential function overlaps with postgis to simplify the diff when comparing before and after upgrade.'
+    createdb citext_db
+    psql -d citext_db -f /usr/local/greenplum-db-source/share/postgresql/contrib/citext.sql
+    psql -v ON_ERROR_STOP=1 -d citext_db <<SQL_EOF
+        CREATE TABLE citext_test_type (
+            id bigint PRIMARY KEY,
+            nick CITEXT NOT NULL,
+            pass TEXT   NOT NULL
+        ) DISTRIBUTED BY (id);
+
+        INSERT INTO citext_test_type VALUES (1,  'larry',  md5(random()::text) );
+SQL_EOF
+"
 
 echo "Running the data migration scripts on the source cluster..."
 ssh -n mdw "
